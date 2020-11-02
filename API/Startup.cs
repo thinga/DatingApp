521f57bb520eafcd26e9ca82ab60a2425d75e87b
anhtuan.nguyen <anhtuan.nguyen@telemedcar.ch>
2020-11-02 16:08:48 +0100

    Ent of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,10 +35,10 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-          services.AddApplicationServices(_config);
+            services.AddApplicationServices(_config);
             services.AddControllers();
             services.AddCors();
-         services.AddIdentityServices(_config);
+            services.AddIdentityServices(_config);
 
         }
 
