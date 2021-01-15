e2fa185178f2a64bce7b22af64caf918d4f8d901
anhtuan.nguyen <anhtuan.nguyen@telemedcar.ch>
2021-01-15 11:53:52 +0100

    User connected + disconnected  Section 17

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -4,6 +4,7 @@ import { from, ReplaySubject } from 'rxjs';
 import { User } from '../_models/user';
 import {map} from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +15,7 @@ export class AccountService {
   currentUsers$ = this.currentUserSource.asObservable();
 
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private presence: PresenceService) { }
 
  
   login(model: any){
@@ -23,6 +24,7 @@ export class AccountService {
         const user = response;
         if (user){
         this.setCurrentUser(user);
+        this.presence.createHubConnection(user);
         }
       })
     );
@@ -34,6 +36,7 @@ export class AccountService {
       map((user: User) => {
         if (user) {    
           this.setCurrentUser(user);
+          this.presence.createHubConnection(user);
         }
       })
     );
@@ -52,6 +55,7 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+    this.presence.stopHubConnection();
   }
 
   getDecodedToken(token){
--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
-  production: false,
-  apiUrl: 'https://localhost:5001/api/'
+  production: true,
+  apiUrl: 'api',
+  hubUrl: 'hubs/'
 };
