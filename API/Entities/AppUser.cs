4ab10845b07d975449bc59363489e26a0981677c
thinga <thnga.tran@gmail.com>
2021-12-02 21:56:20 +0100

    15/182. Setting up the entities for messaging

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -27,6 +27,9 @@ namespace API.Entities
         
         public ICollection<UserLike> LikedUsers { get; set; }
 
+        public ICollection<Message> MessagesSent { get; set; }
+        public ICollection<Message> MessagesReceived { get; set; }
+
       //  public int GetAge()
         //{
           //  return DateOfBirth.CalculateAge();
