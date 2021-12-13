8521ca5eba360b64c972536d24056a6a239e2661
thinga <thnga.tran@gmail.com>
2021-12-13 09:59:24 +0100

    §17/231.Tracking the message groups

--- API/Data/MessageRepository.cs
+++ API/Data/MessageRepository.cs
@@ -1,5 +1,6 @@
 using API.Helpers;
 using AutoMapper.QueryableExtensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
@@ -13,6 +14,11 @@ namespace API.Data
             _context = context;
         }
 
+        public void AddGroup(Group group)
+        {
+           _context.Groups.Add(group);
+        }
+
         public void AddMessage(Message message)
         {
             _context.Messages.Add(message);
@@ -23,12 +29,24 @@ namespace API.Data
             _context.Messages.Remove(message);
         }
 
+        public async Task<Connection> GetConnection(string connectionId)
+        {
+            return await _context.Connections.FindAsync(connectionId);
+        }
+
         public async Task<Message> GetMessage(int id)
         {
             return await _context.Messages
                  .Include(u => u.Sender)
                  .Include(u => u.Recipient)
                  .SingleOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<Group> GetMessageGroup(string groupName)
+        {
+           return await _context.Groups
+                  .Include(x => x.Connections)
+                  .FirstOrDefaultAsync(x=> x.Name == groupName);
         }
 
         public async Task<PagedList<MessageDto>> GetMessagesForUser(MessageParams messageParams)
@@ -83,7 +101,10 @@ namespace API.Data
             return _mapper.Map<IEnumerable<MessageDto>>(messages);
         }
 
-
+        public void RemoveConnection(Connection connection)
+        {
+            _context.Connections.Remove(connection);
+        }
 
         public async Task<bool> SaveAllAsync()
         {
