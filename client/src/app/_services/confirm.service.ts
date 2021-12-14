dc01a578823d168607f83fc468b927576119ae9a
thinga <thnga.tran@gmail.com>
2021-12-14 15:00:18 +0100

    §19/245. Getting the data from the confirmation modal

--- client/src/app/_services/confirm.service.ts
+++ client/src/app/_services/confirm.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { Observable } from 'rxjs';
+import { ConfirmDialogComponent } from '../modals/confirm-dialog/confirm-dialog.component';
 
 @Injectable({
   providedIn: 'root'
@@ -20,6 +22,23 @@ export class ConfirmService {
         btnCancelText
       }
     }
-    this.bsModelRef = this.modalService.show('confirm', config);
+    this.bsModelRef = this.modalService.show(ConfirmDialogComponent, config);
+
+    return new Observable<boolean>(this.getResult());
+  }
+  private getResult() {
+    return (observer) => {
+      const subscription = this.bsModelRef.onHidden.subscribe(() => {
+        observer.next(this.bsModelRef.content.result);
+        observer.complete();
+      });
+
+      return {
+        unsubscribe() {
+          subscription.unsubscribe();
+        }
+      }
+    }
+  }
 }
-}
+
