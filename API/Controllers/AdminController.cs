20eb47470a435cf63665ef199a0bf1988433ce87
thinga <thnga.tran@gmail.com>
2021-12-15 10:22:36 +0100

    PhotoM/6.Implement the AdminController GetPhotosForApproval method

--- API/Controllers/AdminController.cs
+++ API/Controllers/AdminController.cs
@@ -3,31 +3,33 @@ namespace API.Controllers
     public class AdminController : BaseApiController
     {
         private readonly UserManager<AppUser> _userManager;
-        public AdminController(UserManager<AppUser> userManager)
+        private readonly IUnitOfWork _unitOfWork;
+        public AdminController(UserManager<AppUser> userManager, IUnitOfWork unitOfWork)
         {
+            _unitOfWork = unitOfWork;
             _userManager = userManager;
         }
 
         [Authorize(Policy = "RequireAdminRole")]
         [HttpGet("users-with-roles")]
-        public async Task <ActionResult> GetUsersWithRoles()
+        public async Task<ActionResult> GetUsersWithRoles()
         {
-           var users = await _userManager.Users
-               .Include(r => r.UserRoles)
-               .ThenInclude(r => r.Role)
-               .OrderBy(u => u.UserName)
-               .Select(u => new 
-               {
-                   u.Id,
-                   Username = u.UserName,
-                   Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
-               })
-               .ToListAsync();
+            var users = await _userManager.Users
+                .Include(r => r.UserRoles)
+                .ThenInclude(r => r.Role)
+                .OrderBy(u => u.UserName)
+                .Select(u => new
+                {
+                    u.Id,
+                    Username = u.UserName,
+                    Roles = u.UserRoles.Select(r => r.Role.Name).ToList()
+                })
+                .ToListAsync();
 
-               return Ok(users);
+            return Ok(users);
         }
 
-        
+
         [Authorize(Policy = "RequireAdminRole")]
         [HttpPost("edit-roles/{username}")]
 
@@ -37,7 +39,7 @@ namespace API.Controllers
             var user = await _userManager.FindByNameAsync(username);
 
             if (user == null) return NotFound("Could not find user");
-            
+
             var userRoles = await _userManager.GetRolesAsync(user);
             var result = await _userManager.AddToRolesAsync(user, selectedRoles.Except(userRoles));
             if (!result.Succeeded) return BadRequest("Failed to add to roles");
@@ -53,9 +55,10 @@ namespace API.Controllers
         [Authorize(Policy = "ModeratePhotoRole")]
         [HttpGet("photos-to-moderate")]
 
-        public ActionResult GetPhotosForModeration()
+        public async Task<ActionResult> GetPhotosForModeration()
         {
-            return Ok("Admins or moderators can see this");
+           var photos = await _unitOfWork.PhotoRepository.GetUnApprovedPhotos(); 
+           return Ok(photos);
         }
     }
 }
