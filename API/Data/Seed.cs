3d38bb1c24328713df3654a9e2ea77114fd25fe5
thinga <thnga.tran@gmail.com>
2021-12-15 08:35:17 +0100

    PhotoM: Add isApproved to entities, update Dto + Seed

--- API/Data/Seed.cs
+++ API/Data/Seed.cs
@@ -29,13 +29,11 @@ namespace API.Data
             {
                 await roleManager.CreateAsync(role);
             }
-
             
             foreach(var user in users)
             {
-                
+                user.Photos.First().IsApproved = true;
                 user.UserName = user.UserName.ToLower();
-
                 await userManager.CreateAsync(user, "Pa$$w0rd");
                 await userManager.AddToRoleAsync(user, "Member");
             }
