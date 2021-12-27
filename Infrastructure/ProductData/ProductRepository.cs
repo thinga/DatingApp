2538f38e4a928657d0b004801e85e88935d74da4
thinga <thnga.tran@gmail.com>
2021-12-27 22:14:18 +0100

    Skinet/6/Sorting by name, hochPreice,niedrig Preice

--- Infrastructure/ProductData/ProductRepository.cs
+++ Infrastructure/ProductData/ProductRepository.cs
@@ -23,6 +23,7 @@ namespace Infrastructure.ProductData
         }
         public async Task<IReadOnlyList<Product>> GetProductsAsync()
         {
+            
             return await _context.Products
                .Include(p => p.ProductBrand)
                .Include(p => p.ProductType)
