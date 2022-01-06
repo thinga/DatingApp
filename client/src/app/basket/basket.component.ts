7e3fb8069de30458cb166d79678f4d0cba23d07e
thinga <thnga.tran@gmail.com>
2022-01-06 11:52:21 +0100

    shop/14/157. Adding the basket component function

--- client/src/app/basket/basket.component.ts
+++ client/src/app/basket/basket.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
-import { IBasket } from '../_models/basket';
+import { IBasket, IBasketItem } from '../_models/basket';
 import { BasketService } from './basket.service';
 
 @Component({
@@ -9,12 +9,22 @@ import { BasketService } from './basket.service';
   styleUrls: ['./basket.component.scss']
 })
 export class BasketComponent implements OnInit {
-basket$: Observable<IBasket>;
+  basket$: Observable<IBasket>;
 
   constructor(private basketService: BasketService) { }
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
   }
+  removeBasketItem(item: IBasketItem) {
+    this.basketService.removeItemFromBasket(item);
+  }
 
+  incrementItemQuantity(item: IBasketItem) {
+    this.basketService.incrementItemQuantity(item);
+  }
+
+  decrementItemQuantity(item: IBasketItem) {
+    this.basketService.decrementItemQuantity(item);
+  }
 }
